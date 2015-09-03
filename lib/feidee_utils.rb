86303ac5c9ef3d985ca2d38843bbfc099dd2dfb1
Liqing Muyi <muyiliqing@gmail.com>
2015-09-03 22:44:41 +1000

    Add import in the main entrance.

--- lib/feidee_utils.rb
+++ lib/feidee_utils.rb
@@ -3,6 +3,7 @@ require 'feidee_utils/database'
 require 'feidee_utils/record'
 require 'feidee_utils/account'
 require 'feidee_utils/transaction'
+require 'feidee_utils/category'
 
 module FeideeUtils
 end
