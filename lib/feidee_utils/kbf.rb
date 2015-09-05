79133233b56956b25a9d2b966aa063c232ddf980
Liqing Muyi <muyiliqing@gmail.com>
2015-09-05 13:33:09 +1000

    Fix indentation in kbf.rb

--- lib/feidee_utils/kbf.rb
+++ lib/feidee_utils/kbf.rb
@@ -6,31 +6,31 @@ require 'feidee_utils/transaction'
 require 'feidee_utils/account'
 
 module FeideeUtils
-class Kbf
-  DatabaseName = 'mymoney.sqlite'
+  class Kbf
+    DatabaseName = 'mymoney.sqlite'
 
-  attr_reader :zipfile, :sqlite_db
+    attr_reader :zipfile, :sqlite_db
 
-  def initialize(input_stream)
-    @zipfile = Zip::File.open_buffer(input_stream) do |zipfile|
-      zipfile.each do |entry|
-        if entry.name == DatabaseName
-          # Each call to get_input_stream will create a new stream
-          @original_sqlite_db_entry = entry
-          @sqlite_db = FeideeUtils::Database.new(entry.get_input_stream, true)
+    def initialize(input_stream)
+      @zipfile = Zip::File.open_buffer(input_stream) do |zipfile|
+        zipfile.each do |entry|
+          if entry.name == DatabaseName
+            # Each call to get_input_stream will create a new stream
+            @original_sqlite_db_entry = entry
+            @sqlite_db = FeideeUtils::Database.new(entry.get_input_stream, true)
+          end
         end
       end
     end
-  end
 
-  def extract_original_sqlite(dest_file_path = nil)
-    FeideeUtils::Database.feidee_to_sqlite(@original_sqlite_db_entry.get_input_stream, dest_file_path)
-  end
+    def extract_original_sqlite(dest_file_path = nil)
+      FeideeUtils::Database.feidee_to_sqlite(@original_sqlite_db_entry.get_input_stream, dest_file_path)
+    end
 
-  class << self
-    def open_file(file_name)
-      return Kbf.new(File.open(file_name))
+    class << self
+      def open_file(file_name)
+        return Kbf.new(File.open(file_name))
+      end
     end
   end
-end
 end
