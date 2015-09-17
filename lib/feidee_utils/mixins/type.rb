fe212eed098ca57b0774759c06be7563f24a696b
Liqing Muyi <muyiliqing@gmail.com>
2015-09-18 09:12:28 +1000

    Support reverse lookup.

--- lib/feidee_utils/mixins/type.rb
+++ lib/feidee_utils/mixins/type.rb
@@ -6,6 +6,19 @@ module FeideeUtils
       module ClassMethods
         def define_type_enum type_enum
           const_set :TypeEnum, type_enum.freeze
+
+          enum_values = type_enum.values
+          type_code = if enum_values.size == enum_values.uniq.size
+            type_enum.invert
+          else
+            {}
+          end
+
+          const_set :TypeCode, type_code.freeze
+        end
+
+        def type_code type_enum
+          self::TypeCode[type_enum]
         end
       end
 
@@ -16,7 +29,6 @@ module FeideeUtils
       def type
        self.class::TypeEnum[raw_type]
       end
-      # TODO: support reverse lookup.
     end
   end
 end
--- test/feidee_utils/mixins/type_test.rb
+++ test/feidee_utils/mixins/type_test.rb
@@ -25,9 +25,29 @@ class FeideeUtils::Mixins::TypeTest < MiniTest::Test
     assert_equal :xyz, @type1.type
   end
 
+  def test_type_code
+    assert_equal 0, (TestClass.type_code :abc)
+    assert_equal 1, (TestClass.type_code :xyz)
+  end
+
   def test_define_const
     refute_nil TestClass::TypeEnum
+    refute_nil TestClass::TypeCode
     assert_equal :abc, TestClass::TypeEnum[0]
     assert_equal :xyz, TestClass::TypeEnum[1]
+    assert_equal 0, TestClass::TypeCode[:abc]
+    assert_equal 1, TestClass::TypeCode[:xyz]
+  end
+
+  def test_duplicate_enum
+    klass = Class.new do
+      include FeideeUtils::Mixins::Type
+      define_type_enum({
+        0 => :abc,
+        1 => :xyz,
+        2 => :abc,
+      })
+    end
+    assert_equal ({}), klass::TypeCode
   end
 end
