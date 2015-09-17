73585818a9db3de193803f008466dff10c3e9188
Liqing Muyi <muyiliqing@gmail.com>
2015-09-17 12:32:25 +1000

    Add raw fields tests for category.

--- test/feidee_utils/category_test.rb
+++ test/feidee_utils/category_test.rb
@@ -7,9 +7,31 @@ class FeideeUtils::CategoryTest < MiniTest::Test
     @sqlite_db = FeideeUtils::TestUtils.open_test_sqlite
 
     @expenditure = @sqlite_db.namespaced::Category.find_by_id(-1)
-    @meals = @sqlite_db.namespaced::Category.find_by_id(-18)
+    @meals = @sqlite_db.namespaced::Category.find_by_id(-16)
     @income = @sqlite_db.namespaced::Category.find_by_id(-56)
     @salary = @sqlite_db.namespaced::Category.find_by_id(-57)
+
+    @fruit = @sqlite_db.namespaced::Category.find_by_id(-18)
+  end
+
+  def test_fields
+    assert_equal "水果零食", @fruit.name
+    assert_equal "职业收入", @salary.name
+
+    assert_equal (-3), @fruit.parent_poid
+    assert_equal (-56), @salary.parent_poid
+
+    assert_equal "/-1/-3/-18/", @fruit.raw_path
+    assert_equal "/-56/-57/", @salary.raw_path
+
+    assert_equal 2, @fruit.depth
+    assert_equal 1, @salary.depth
+
+    assert_equal 0, @fruit.raw_type
+    assert_equal 1, @salary.raw_type
+
+    assert_equal 3, @fruit.ordered
+    assert_equal 1, @salary.ordered
   end
 
   def test_name
